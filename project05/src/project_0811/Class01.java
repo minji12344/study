4d2ffbb8906fd8c9645d119692fc0f26ca3f1662
minji12344 <alswl12344@naver.com>
2023-08-11 10:13:16 +0900

    0811

---
+++ project05/src/project_0811/Class01.java
@@ -0,0 +1,27 @@
+package project_0811;
+
+public class Class01 implements fly{
+
+	public static void main(String[] args) {
+		
+
+	}
+
+	@Override
+	public void fly() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
+// 추상클래스
+// extends ->  추상 메서드를 무조건 하나 가져야 한다. 메서드, 변수
+
+// 인터페이스 
+// 구현된 메서드나 변수가 없으며, 오로지 추상 메서드와 상수만을 가지고 있다.
+// implements
+// 부모는 하나만, 인터페이스는 여러개(다중 상속)
+// 프렌차이즈 계약서 -> 필수 요소(식당명, 메뉴, 레시피)
+
+// 
---
+++ project05/src/project_0811/fly.java
@@ -0,0 +1,6 @@
+package project_0811;
+
+public interface fly {
+	final static int fly = 1;
+	abstract void fly();
+}
